9e7a507b8b29891983aa427d8f9cffb3b3b264e3
Isterdam <adrian.murphy99@mail.com>
2020-11-11 22:01:24 +0100

    Formatting

--- src/Student.java
+++ src/Student.java
@@ -23,8 +23,8 @@ public class Student {
 	
 	private double round(double value, int places) {
 		long factor = (long) Math.pow(10, places);
-	    value = value * factor;
-	    long tmp = Math.round(value);
-	    return (double) tmp / factor;
+		value = value * factor;
+		long tmp = Math.round(value);
+		return (double) tmp / factor;
 	}
 }
