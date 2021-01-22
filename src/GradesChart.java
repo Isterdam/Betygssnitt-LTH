62bd72a7a051bf69f48af80e02d6c88de82fef15
Isterdam <adrian.murphy99@mail.com>
2021-01-23 00:00:38 +0100

    added dates

---
+++ src/GradesChart.java
@@ -0,0 +1,31 @@
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartPanel;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.block.BlockBorder;
+import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.chart.plot.XYPlot;
+import org.jfree.chart.renderer.xy.XYLineAndShapeRenderer;
+import org.jfree.chart.title.TextTitle;
+import org.jfree.data.xy.XYDataset;
+import org.jfree.data.xy.XYSeries;
+import org.jfree.data.xy.XYSeriesCollection;
+
+import javax.swing.BorderFactory;
+import javax.swing.JFrame;
+import java.awt.BasicStroke;
+import java.awt.Color;
+import java.awt.EventQueue;
+import java.awt.Font;
+import java.util.ArrayList;
+
+
+public class GradesChart extends JFrame {
+	
+	public GradesChart(ArrayList<Course> courses) {
+		makeChart(courses);
+	}
+	
+	private void makeChart(ArrayList<Course> courses) {
+		
+	}
+}
